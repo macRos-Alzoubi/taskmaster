a5b75d7c88e1f7e000a0c6afbc028b11078b7690
macRos-Alzoubi <macRos.alzoubi@gmail.com>
2021-11-18 19:01:40 +0200

    fin lab-38

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifyDatasource": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "taskmaster467df5e2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "storage": {
+        "taskMasterStore": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
